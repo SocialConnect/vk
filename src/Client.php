a9a96ce759ce60c29dd157c0ad071913c24e8e77
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-11 06:22:08 +0400

    Write Client

--- src/Client.php
+++ src/Client.php
@@ -27,11 +27,62 @@ class Client
      */
     protected $httpClient;
 
+    protected $baseParameters = array(
+        'v' => 5.24
+    );
+
     public function __construct($appId, $appSecret)
     {
         $this->appId = $appId;
         $this->appSecret = $appSecret;
 
-        $this->httpClient = new \Guzzle\Http\Client();
+
+
+        $this->httpClient = new \Guzzle\Http\Client('https://api.vk.com/');
+    }
+
+    /**
+     * @param $uri
+     * @param array $parameters
+     * @return bool
+     */
+    public function request($uri, array $parameters = array())
+    {
+        $request = $this->httpClient->get($uri.'?'.http_build_query($parameters));
+        $response = $request->send();
+
+        if ($response) {
+            if ($response->isServerError()) {
+
+            }
+
+            $body = $response->getBody(true);
+            if ($body) {
+                $json = json_decode($body);
+
+                if ($json->response) {
+                    return $json->response[0];
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function getUser($id)
+    {
+        $result = $this->request('method/getProfiles', array(
+            'user_id' => $id
+        ));
+
+        if ($result) {
+
+        }
+
+        return false;
     }
 }
