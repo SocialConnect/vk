33bf6d2479a6350c42bf6236f5de37b9474d9f6c
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-11 05:58:03 +0400

    Start writting client

--- src/Client.php
+++ src/Client.php
@@ -8,5 +8,30 @@ namespace SocialConnect\Vk;
 
 class Client
 {
+    /**
+     * Application secret
+     *
+     * @var string|integer
+     */
+    protected $appId;
 
+    /**
+     * Application secret
+     *
+     * @var string
+     */
+    protected $appSecret;
+
+    /**
+     * @var \Guzzle\Http\Client
+     */
+    protected $httpClient;
+
+    public function __construct($appId, $appSecret)
+    {
+        $this->appId = $appId;
+        $this->appSecret = $appSecret;
+
+        $this->httpClient = new \Guzzle\Http\Client();
+    }
 }
