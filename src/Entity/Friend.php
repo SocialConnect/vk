538cda2c6cf620d4e5322be5ad7d4396823b2a7e
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-16 22:03:29 +0400

    Add Friend entity

---
+++ src/Entity/Friend.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry @ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Vk\Entity;
+
+/**
+ * Class User
+ * @package SocialConnect\Vk\Entity
+ */
+class Friend extends User
+{
+
+}
